57d4d8ab39a695c26856e91b34e3f16da6d7e2c2
Vaibhav Singh <94214732+v0nser@users.noreply.github.com>
2025-07-02 23:48:37 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://inventory-mgmt-kh3o.onrender.com',
     },
   },
 })
