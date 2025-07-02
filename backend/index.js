97f39dec587ffdb8bd2629d63a2e0aeeba836177
Vaibhav Singh <94214732+v0nser@users.noreply.github.com>
2025-07-02 23:54:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://inventory-mgmt-one.vercel.app', credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
